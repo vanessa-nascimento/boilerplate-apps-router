f04235adf4b5ee38e0f62b7425231b2664dca4cd
Vanessa Nascimento <vanessa.nascimento@jusbrasil.com.br>
2024-01-04 15:30:32 -0300

    config: initial jest tests

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('shoul render the heading', () => {
+    const { container } = render(<Main />)
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
